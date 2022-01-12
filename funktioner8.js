758f27f9699904345f16956bb32947b2a7f58fc1
dansch1982 <84453251+dansch1982@users.noreply.github.com>
2022-01-12 09:39:30 +0100

    Update funktioner8.js

--- funktioner8.js
+++ funktioner8.js
@@ -1,3 +1,9 @@
 function eight() {
-    
+    const output = document.getElementById(arguments.callee.name)
+    eight.textContent = ""
+    let number = prompt('Skriv in ett tal')
+    while (number > 2) {
+        number = number % 2 === 0 ? number / 2 : number * 3 + 1
+        output.textContent += number
+    }
 }
