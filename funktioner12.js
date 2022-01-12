57ece5ee2d339aa7844651b0c2636f3f4361c800
dansch1982 <84453251+dansch1982@users.noreply.github.com>
2022-01-12 19:11:16 +0100

    Update funktioner12.js

--- funktioner12.js
+++ funktioner12.js
@@ -1,3 +1,13 @@
 function twelve(amount) {
     const output = document.getElementById(arguments.callee.name)
+
+    if (isNaN(amount)) {
+        return output.textContent = `${amount} är ingen siffra.`
+    }
+    amount = Math.floor(amount)
+    let number = 0;
+    for (let index = 0; index < amount; index++) {
+        number += index + 1
+    }
+    output.textContent = number
 }
