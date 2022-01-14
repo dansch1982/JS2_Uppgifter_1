ee0cf254854178e13cd8b0f616a993511e7e7421
dansch1982 <84453251+dansch1982@users.noreply.github.com>
2022-01-14 08:42:48 +0100

    Update funktioner15.js

--- funktioner15.js
+++ funktioner15.js
@@ -1,9 +1,9 @@
 function fifteen(input) {
-    const output = document.getElementById(arguments.callee.name)
 
-    input = Math.floor(input)
-    
-    //kod
-
+    input = isNaN(Math.floor(input)) ? Math.floor(input) : fibonacci(Math.floor(input))
     output.textContent = input
+}
+
+function fibonacci(number) {
+    return number < 2 ? number : fibonacci(number - 1) + fibonacci(number - 2)
 }
